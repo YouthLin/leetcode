a396b067b3a9358026b9eb86739f2c1433ff7402
youthlin.chen <yulinlin1995@gmail.com>
2020-06-01 21:20:19 +0800

    旋转数组

--- src/main/java/com/youthlin/leetcode/arrays/Arrays.java
+++ src/main/java/com/youthlin/leetcode/arrays/Arrays.java
@@ -74,4 +74,42 @@ public class Arrays {
         nums[index] = value;
     }
 
+    /**
+     * 旋转数组：给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/rotate-array/solution/xuan-zhuan-shu-zu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public static void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        k %= nums.length;
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    /**
+     * 假设 n=7n=7 且 k=3 。
+     * <p>
+     * 原始数组                  : 1 2 3 4 5 6 7
+     * 反转所有数字后             : 7 6 5 4 3 2 1
+     * 反转前 k 个数字后          : 5 6 7 4 3 2 1
+     * 反转后 n-k 个数字后        : 5 6 7 1 2 3 4 --> 结果
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/rotate-array/solution/xuan-zhuan-shu-zu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
 }
